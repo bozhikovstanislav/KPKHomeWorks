84ae14c10a7f59e4c58244578b9fd2e85a72e796
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2015-07-18 19:01:29 +0300

    04HomeWork

---
+++ Documntation/TestDocumentationClass.cs
@@ -0,0 +1,23 @@
+﻿namespace Documntation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class TestDocumentationClass
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <returns></returns>
+        static long sum(int firstNumber,int secondNumber)
+        {
+            long sum = 0;
+            return sum = firstNumber + secondNumber;
+        }
+    }
+}
